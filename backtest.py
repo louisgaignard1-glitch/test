3e6c24665676616d8b608253fd8e73d5f7d90726
louisgaignard1-glitch <louisgaignard1@gmail.com>
2026-02-23 17:24:37 +0100

    Update backtest.py

--- backtest.py
+++ backtest.py
@@ -1,13 +1,6 @@
-
-import pandas as pd
-
 def backtest_portfolio(data, allocation):
     returns = data.pct_change().dropna()
-
-    # alignement des poids
     weights = allocation['Poids'].reindex(returns.columns).fillna(0)
-
     portfolio_returns = returns.dot(weights)
     cumulative_returns = (1 + portfolio_returns).cumprod()
-
     return cumulative_returns
