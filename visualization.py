e3a1349a1d75af183b3e8efbbe6ee85e757ed78a
louisgaignard1-glitch <louisgaignard1@gmail.com>
2026-03-05 23:40:29 +0100

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -42,20 +42,59 @@ def plot_efficient_frontier(result_min_var, mu, Sigma, assets):
     st.plotly_chart(fig)
 
 
-def plot_sector_allocation(allocation):
+
+
+    def plot_sector_allocation(allocation):
 
     sectors = {
         "ENGI.PA": "Énergie",
         "BNP.PA": "Finance",
-        "ACA.PA": "Industrie",
-        "GLE.PA": "Énergie",
+        "ACA.PA": "Finance",
+        "GLE.PA": "Finance",
         "TTE.PA": "Énergie",
         "MC.PA": "Luxe",
-        "OR.PA": "Mines",
-        "AIR.PA": "Industrie",
+        "OR.PA": "Consommation",
+        "AIR.PA": "Aéronautique",
         "RNO.PA": "Automobile",
-        "VK.PA": "Technologie"
+        "VK.PA": "Industrie",
+
+        "KER.PA": "Luxe",
+        "RMS.PA": "Luxe",
+
+        "SAF.PA": "Aéronautique",
+        "HO.PA": "Défense",
+
+        "SU.PA": "Industrie",
+        "CAP.PA": "Technologie",
+        "STMPA.PA": "Technologie",
+
+        "EDF.PA": "Énergie",
+        "VIE.PA": "Environnement",
+        "EN.PA": "Construction",
+
+        "SAN.PA": "Santé",
+
+        "SGO.PA": "Matériaux",
+
+        "ORA.PA": "Télécom",
+
+        "CA.PA": "Distribution",
+        "RI.PA": "Consommation",
+
+        "DG.PA": "Construction",
+        "AI.PA": "Industrie"
     }
+
+    sector_allocation = allocation.copy()
+
+    sector_allocation["Secteur"] = sector_allocation.index.map(sectors)
+
+    # si un ticker n'est pas reconnu
+    sector_allocation["Secteur"] = sector_allocation["Secteur"].fillna("Autre")
+
+    sector_allocation = sector_allocation.groupby("Secteur")["Poids"].sum()
+
+    st.bar_chart(sector_allocation)
 
     sector_allocation = allocation.assign(
         Secteur=lambda x: x.index.map(sectors)
