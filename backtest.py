5259f453d8a25ef3897378d8be2dae1ecb51cade
louisgaignard1-glitch <louisgaignard1@gmail.com>
2026-03-20 23:16:34 +0100

    Update backtest.py

--- backtest.py
+++ backtest.py
@@ -1,14 +1,14 @@
 def backtest_portfolio(data, allocation):
-
     returns = data.pct_change().dropna()
-
     weights = allocation['Poids'].reindex(returns.columns).fillna(0)
-
     portfolio_returns = returns.dot(weights)
+    cumulative = (1 + portfolio_returns).cumprod()
 
-    cumulative_returns = (1 + portfolio_returns).cumprod()
+
+    annualized_return = portfolio_returns.mean() * 252
+    annualized_vol = portfolio_returns.std() * np.sqrt(252)
     
-    benchmark = yf.download("^FCHI", start=start_date, auto_adjust=True)["Close"]
-    benchmark_returns = (1 + benchmark.pct_change().dropna()).cumprod()
-
-    return cumulative_returns
+    return cumulative, annualized_return, annualized_vol
+    
+benchmark = yf.download("^FCHI", start=start_date, auto_adjust=True)["Close"]
+benchmark_returns = (1 + benchmark.pct_change().dropna()).cumprod()
